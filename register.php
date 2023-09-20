7a56d2fa676db28d115ca08d57dab040f6d12dd3
Rhitajyoti Mandal <rhitajyotim411@gmail.com>
2023-09-20 16:35:06 +0530

    Added CSS to register

--- register.php
+++ register.php
@@ -9,27 +9,62 @@ session_start();
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <title>Register</title>
+  <!-- Bootstrap 5 -->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet">
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js"></script>
+
+  <style>
+    .half {
+      background-color: #EBFFFE;
+      border-radius: 5px;
+    }
+
+    .half:hover {
+      background-color: #ffffff;
+      border-radius: 5px;
+    }
+
+    h1,
+    h5,
+    p {
+      color: #08314A !important;
+    }
+  </style>
 </head>
 
-<body>
-  <?php
-  if (isset($_SESSION['UID'])) {
-    echo "{$_SESSION['UID']} already logged in<br>";
-    echo "Redirecting to dashboard...";
-    die(header("refresh:2; URL=./{$_SESSION['type']}/dashboard.php"));
-  }
-  ?>
-  <p>Register as: - </p>
-  <ul>
-    <li>
-      <a href="admin/register.php">Admin</a>
-    </li>
-    <li>
-      <a href="staff/register.php">Staff</a>
-    </li>
-  </ul>
+<?php
+if (isset($_SESSION['UID'])) {
+  echo "{$_SESSION['UID']} already logged in<br>";
+  echo "Redirecting to dashboard...";
+  die(header("refresh:2; URL=./{$_SESSION['type']}/dashboard.php"));
+}
+?>
 
-  Already an user? Login <a href="index.php">here</a>.
+<body style="background-color: #7FEDDF">
+  <div class="container-fluid text-center" style="height: 100vh;">
+    <div class="row justify-content-md-center mt-5">
+      <h1>Register as: - </h1>
+      <div class="col-10 col-md-4 mt-5 mb-4">
+        <div class="card">
+          <div class="card-body half">
+            <h5 class="card-title">Admin</h5>
+            <p class="card-text">Register as an Admin</p>
+            <a href="admin/register.php" class="btn btn-primary">Admin Registration</a>
+          </div>
+        </div>
+      </div>
+      <div class="col-10 col-md-4 mt-5 mb-4">
+        <div class="card">
+          <div class="card-body half">
+            <h5 class="card-title">Staff</h5>
+            <p class="card-text">Register as a Staff</p>
+            <a href="staff/register.php" class="btn btn-primary">Staff Registration</a>
+          </div>
+        </div>
+      </div>
+      <p>Already an user? Login <a href="index.php">here</a></p>
+    </div>
+  </div>
 </body>
 
 </html>
