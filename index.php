9aa8fe56f5a21593f8d619a62dd81ab10a860c6f
Rhitajyoti Mandal <rhitajyotim411@gmail.com>
2023-09-20 16:55:06 +0530

    Added navbar prototype

--- index.php
+++ index.php
@@ -12,24 +12,8 @@ session_start();
   <!-- Bootstrap 5 -->
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet">
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js"></script>
-
-  <style>
-    .half {
-      background-color: #EBFFFE;
-      border-radius: 5px;
-    }
-
-    .half:hover {
-      background-color: #ffffff;
-      border-radius: 5px;
-    }
-
-    h1,
-    h5,
-    p {
-      color: #08314A !important;
-    }
-  </style>
+  <!-- CSS -->
+  <link href="style/log_reg.css" rel="stylesheet">
 </head>
 
 <?php
@@ -40,10 +24,11 @@ if (isset($_SESSION['UID'])) {
 }
 ?>
 
-<body style="background-color: #7FEDDF">
+<body>
+  <?php require 'inc/header.php' ?>
   <div class="container-fluid text-center" style="height: 100vh;">
     <div class="row justify-content-md-center mt-5">
-      <h1>Log in to: - </h1>
+      <h1>Log in to: -</h1>
       <div class="col-10 col-md-4 mt-5 mb-4">
         <div class="card">
           <div class="card-body half">
